5b489f20d919dcd6bd9f82740c2e3f8998cdf82a
Miguel Pons <miguelponsanchez@gmail.com>
2020-05-07 15:05:07 +0200

    Creacio bàsica de les pàgines per Alumne, Dies, Login i Professor

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-avatar>
+          <img src="~/assets/esliceu-logo.png">
+        </q-avatar>
+        <q-toolbar-title>
+          Menjador App
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+    import EssentialLink from 'components/EssentialLink'
+
+    export default {
+        name: 'LoginLayout',
+
+        components: {
+            EssentialLink
+        },
+
+        data () {
+            return {
+            }
+        }
+    }
+</script>
