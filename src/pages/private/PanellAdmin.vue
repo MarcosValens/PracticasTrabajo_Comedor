bb8ba62ce0bc0b18ca2a4d82d3814fb07b85c7c9
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-05-18 11:58:10 +0200

    Avanzamos con el panel de administracion

--- src/pages/private/PanellAdmin.vue
+++ src/pages/private/PanellAdmin.vue
@@ -1,42 +1,70 @@
 <template>
-  <q-page>
+  <q-page class="q-pa-xl">
 
-    <div class="row justify-center">
-
-      <template>
-        <div class="q-pa-md">
-          <q-uploader
-            color="primary"
-            label="Pujar el XML"
-            auto-upload
-            :url="urlSubirXML"
-            @uploaded="xmlSubido"
-            method="PUT"
-            style="max-width: 260px"
-            fieldName="file"
-            :max-file-size="10485760"
-          />
-        </div>
-      </template>
+    <div class="row flex justify-end q-pa-sm">
+      <q-btn color="primary" @click="uploadXML=!uploadXML" unelevated label="Otra accion posible"
+             icon="far fa-file-excel" class="q-mx-xs"/>
+      <q-btn color="primary" @click="uploadXML=!uploadXML" unelevated label="Actualizar datos CORE via XML"
+             icon="far fa-file-excel" class="q-mx-xs"/>
+    </div>
+    <div class="row bg-indigo q-mt-sm q-pa-sm">
+      f
     </div>
 
+
+    <q-dialog v-model="uploadXML">
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">
+            Actualizar datos
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-file
+            v-model="fileXml"
+            label="Escoger archivo (xml)"
+            outlined
+            use-chips
+            style="max-width: 300px"
+            accept=".xml"
+          >
+            <template v-slot:after>
+              <q-btn
+                :disable="fileXml==null || fileXml.length==0"
+                color="primary"
+                dense
+                icon="cloud_upload"
+                round
+                :loading="uploadingXml"
+
+                @click="uploadXml"
+              />
+            </template>
+          </q-file>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script>
   export default {
     name: "PanellAdmin",
-    data(){
-      return{
-        urlSubirXML:"http://localhost:8080/uploadxml"
+    data() {
+      return {
+        fileXml: null,
+        uploadXML: false,
+        uploadingXml: false
       }
     },
-    methods:{
-      xmlSubido(){
+    methods: {
+      async uploadXml() {
+        this.uploadingXml = true;
 
-        console.log("XML subido");
+        this.uploadingXml = false;
       }
     }
+
   }
 </script>
 
