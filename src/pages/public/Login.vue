f017a4e94fb37135b2ce00c1e101558b6e070dc7
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-05-19 15:28:13 +0200

    Añadimos la url en el properties y preparamos el link que hará el oatuh

--- src/pages/public/Login.vue
+++ src/pages/public/Login.vue
@@ -23,7 +23,10 @@
             </q-input>
           </q-card-section>
           <q-card-actions class="q-px-md">
-            <q-btn unelevated color="primary" size="lg" class="full-width" label="Login"  @click="doLogin"/>
+            <q-btn unelevated color="primary" size="lg" class="full-width" label="Login" @click="doLogin"/>
+            <a :href="urlLoginOauth">
+              <q-btn unelevated color="primary" size="lg" class="full-width" label="Login" @click="doLogin"/>
+            </a>
           </q-card-actions>
 
         </q-card>
@@ -38,7 +41,8 @@
     name: 'Login',
     data () {
       return {
-        login:{
+        urlLoginOauth: process.env.CORE_URL + process.env.URI_OAUTH_GOOGLE,
+        login: {
           email: '',
           password: '',
         },
