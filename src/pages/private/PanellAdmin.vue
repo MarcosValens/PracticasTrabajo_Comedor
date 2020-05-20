890557babc7f7dbf0bda9663f31357d80be935ed
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-05-20 10:15:30 +0200

    Vamos preparando todo lo que es la pantalla de administracion y todas las peticiones aun que aun no sean reales

--- src/pages/private/PanellAdmin.vue
+++ src/pages/private/PanellAdmin.vue
@@ -2,7 +2,7 @@
   <q-page class="q-pa-xl">
 
     <div class="row flex justify-between q-pa-sm">
-      <q-input outlined dense debounce="300" placeholder="Search">
+      <q-input outlined dense debounce="300" placeholder="Search" @input="filterProfesor" v-model="filtroProfesor">
         <template v-slot:prepend>
           <q-icon name="search"/>
         </template>
@@ -16,19 +16,21 @@
     </div>
     <div class="row  q-mt-sm q-pa-sm">
       <q-table
-        class="full-width" :data="dataUsers" :columns="columns" row-key="name" separator="cell" :pagination="{
-          rowsPerPage: 0
-        }" :rows-per-page-options="[]"
+        class="full-width" :data="dataProfesoresFiltered" :columns="columns" row-key="name" separator="cell"
+        :pagination="{
+          rowsPerPage: 12
+        }" :rows-per-page-options="[0,12,15]"
       >
         <template v-slot:body="props">
           <q-tr :props="props">
-
-            <q-td key="nombre" :props="props">{{props.row.name}}</q-td>
+            <q-td key="username" :props="props">{{props.row.username}}</q-td>
+            <q-td key="nom" :props="props">{{props.row.nom}}</q-td>
             <q-td key="apellido" :props="props">{{props.row.ap1}}</q-td>
             <q-td key="apellido2" :props="props">{{props.row.ap2}}</q-td>
             <q-td key="email" :props="props">
               {{props.row.email}}
-              <q-popup-edit v-model="props.row.email" label-set="Asignar email" label-cancel="Cancelar" buttons>
+              <q-popup-edit v-model="props.row.email" label-set="Asignar email" label-cancel="Cancelar" buttons
+                            @save="asignarEmail(props.row)">
                 <q-input outlined label="email" v-model="props.row.email" dense autofocus/>
               </q-popup-edit>
             </q-td>
@@ -82,37 +84,40 @@
     async created() {
 
       this.$q.loading.show()
-      const response = await this.$axiosCore.get('/private/profesores');
-      if (response.status == 200) {
-        this.dataUsers = response.data;
-      }else{
+      const response = await this.$axiosCore.get('/private/professor');
+      if (response.status === 200) {
+        this.dataProfesores = response.data;
+        this.dataProfesoresFiltered = this.dataProfesores;
+      } else {
         this.notify(response.data)
       }
       this.$q.loading.hide()
 
 
-
     },
     data() {
       return {
+        filtroProfesor: '',
         fileXml: null,
         uploadXML: false,
         uploadingXml: false,
         columns: [
           {
-            name: 'nombre',
+            name: 'nom',
             required: true,
             label: 'Nombre',
             align: 'left',
-            field: row => row.name,
+            field: row => row.nom,
             sortable: true,
           },
           {name: 'apellido', align: 'left', label: 'Apellido', field: row => row.ap1, sortable: true},
           {name: 'apellido2', align: 'left', label: 'Segundo apellido', field: row => row.ap2, sortable: true},
+          {name: 'username', align: 'center', label: 'Username', field: row => row.username, sortable: true},
           {name: 'email', align: 'center', label: 'Email', field: row => row.email, sortable: true},
 
         ],
-        dataUsers: null
+        dataProfesores: [],
+        dataProfesoresFiltered: null
       }
     },
     methods: {
@@ -121,7 +126,7 @@
         const formData = new FormData()
         formData.append('file', this.fileXml)
 
-        const response = await this.$axiosCore.put('/uploadxml', formData)
+        const response = await this.$axiosCore.put('/private/uploadxml', formData)
         if (response.status === 200) {
           this.notify("XML  cargado correctamente en la base de datos")
         } else {
@@ -133,9 +138,36 @@
       notify(message) {
         this.$q.notify({
           message: message,
-          color: 'primary',
+          color: 'secondary',
           position: 'bottom-left'
         })
+      },
+      filterProfesor(texto) {
+        this.dataProfesoresFiltered = this.dataProfesores.filter(profesor => {
+
+          if (profesor.username.toLowerCase().includes(texto.toLowerCase())) return true;
+          if (profesor.email != null && profesor.email.toLowerCase().includes(texto.toLowerCase())) return true;
+          const nombreCompleto = profesor.nom + ' ' + profesor.ap1 + ' ' + profesor.ap2;
+          return nombreCompleto.toLowerCase().includes(texto.toLowerCase());
+        })
+      },
+      async asignarEmail(profesor) {
+        const codigo = profesor.codi;
+        const email = profesor.email;
+        if (codigo && email) {
+
+          const response = await this.$axiosCore.put('/admin/professor/email', {
+            email: email,
+            codigo: codigo
+          })
+
+          if (response.status === 200) {
+            this.notify("Email asignado correctamente")
+          } else {
+            this.notify(response.data);
+          }
+
+        }
       }
     }
 
