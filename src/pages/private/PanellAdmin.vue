8170c5049636fb083767ea25418dc2781d7100a6
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-05-22 11:18:55 +0200

    Empezamos cambiando la lista de profesores del panel a la pantalla de profesores

--- src/pages/private/PanellAdmin.vue
+++ src/pages/private/PanellAdmin.vue
@@ -35,47 +35,6 @@
     <div class="row   q-pa-sm">
 
 
-      <q-table
-        class="full-width" :data="dataProfesoresFiltered" :columns="columnsProfesores" row-key="name" separator="cell"
-      >
-        <template v-slot:top class="bg-indigo">
-          <div :class="$q.screen.gt.md?'full-width flex justify-between':'full-width'">
-            <div class="text-h5">Professors</div>
-            <q-input outlined dense debounce="300" placeholder="Cercar" @input="filterProfesor"
-                     v-model="filtroProfesor">
-              <template v-slot:append>
-                <q-icon name="search"/>
-              </template>
-            </q-input>
-
-          </div>
-        </template>
-
-        <template v-slot:body="props">
-
-          <q-tr :props="props">
-            <q-td key="username" :props="props">{{props.row.username}}</q-td>
-            <q-td key="nom" :props="props">{{props.row.nom}}</q-td>
-            <q-td key="apellido" :props="props">{{props.row.ap1}}</q-td>
-            <q-td key="apellido2" :props="props">{{props.row.ap2}}</q-td>
-            <q-td key="email" :props="props" style="max-width: 200px">
-              <q-input dense v-model="props.row.email" outlined label="Asignar email" class="full-width">
-                <template v-slot:append>
-                  <q-btn class="q-mx-xs" round size="sm" color="green-9" icon="fas fa-save"
-                         @click="asignarEmail((props.row))"
-                         :disable="!(props.row.email!==undefined && props.row.email!=='')"
-                         v-show="(props.row.email!==undefined && props.row.email!=='')"/>
-                  <q-btn outline class="q-mx-xs" round size="sm" color="red-9" icon="far fa-trash-alt"
-                         @click="removeEmailUser(props.row)"
-                         :disable="!(props.row.email!==undefined && props.row.email!=='')"
-                         v-show="(props.row.email!==undefined && props.row.email!=='')"/>
-                </template>
-              </q-input>
-            </q-td>
-          </q-tr>
-        </template>
-      </q-table>
-
     </div>
 
 
@@ -173,21 +132,10 @@
     name: "PanellAdmin",
     async created() {
 
-      this.$q.loading.show()
-      const response = await this.$axiosCore.get('/private/professor');
-      if (response.status === 200) {
-        this.dataProfesores = response.data;
-        this.dataProfesoresFiltered = this.dataProfesores;
-      } else {
-        this.notify(response.data)
-      }
-      this.$q.loading.hide()
-
 
     },
     data() {
       return {
-        filtroProfesor: '',
         fileXml: null,
         uploadXML: false,
         uploadingXml: false,
@@ -201,24 +149,8 @@
           roles: [],
           showContrasenya: false
         },
-        columnsProfesores: [
-          {name: 'username', align: 'center', label: 'Nom d\'usuari', field: row => row.username, sortable: true},
 
-          {
-            name: 'nom',
-            required: true,
-            label: 'Nom',
-            align: 'left',
-            field: row => row.nom,
-            sortable: true,
-          },
-          {name: 'apellido', align: 'left', label: 'Cognom', field: row => row.ap1, sortable: true},
-          {name: 'apellido2', align: 'left', label: 'Segon cognom', field: row => row.ap2, sortable: true},
-          {name: 'email', align: 'center', label: 'Email', field: row => row.email, sortable: true},
 
-        ],
-        dataProfesores: [],
-        dataProfesoresFiltered: []
       }
     },
     methods: {
@@ -243,51 +175,7 @@
           position: 'bottom-left'
         })
       },
-      filterProfesor(texto) {
-        this.dataProfesoresFiltered = this.dataProfesores.filter(profesor => {
 
-          if (profesor.username.toLowerCase().includes(texto.toLowerCase())) return true;
-          if (profesor.email != null && profesor.email.toLowerCase().includes(texto.toLowerCase())) return true;
-          const nombreCompleto = profesor.nom + ' ' + profesor.ap1 + ' ' + profesor.ap2;
-          return nombreCompleto.toLowerCase().includes(texto.toLowerCase());
-        })
-      },
-      async asignarEmail(profesor) {
-        const codigo = profesor.codi;
-        const email = profesor.email;
-        if (codigo && email) {
-
-          const response = await this.$axiosCore.put('/admin/professor/email', {
-            email: email,
-            codigo: codigo
-          })
-
-          if (response.status === 200) {
-            this.notify("Email asignado correctamente")
-          } else {
-            this.notify(response.data);
-          }
-
-        }
-      },
-      async removeEmailUser(profesor) {
-        const codigo = profesor.codi;
-
-        // Aqui hacemos que el email no se vea mas en la lista
-        profesor.email = undefined
-
-        const response = await this.$axiosCore.delete('/admin/professor/email', {
-          data: {
-            codi: codigo
-          }
-        })
-
-        if (response.status === 200) {
-          this.notify("Email eliminado correctamente")
-        } else {
-          this.notify(response.data);
-        }
-      },
       clearNewUsuari() {
         this.usuarioToAdd.nombre = '';
         this.usuarioToAdd.apellido1 = '';
